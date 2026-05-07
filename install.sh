5c39bf85b7c52d6bf04039cb7e8710063afed12f
mun luna <115883618+mun-luna@users.noreply.github.com>
2026-05-07 17:24:17 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "THIS SCRIPT WILL NOT INSTALL THE NEEDED PACKAGES: which are fcitx5, fcitx5-table-other, and sometimes fcitx5-chinese-addons depending on the distro."
 echo "copying files..."
 cp ./nasin-mun/inputmethod/nasin-mun.conf ~/.local/share/fcitx5/inputmethod/nasin-mun.conf
 cp ./nasin-mun/punctuation/punc.mb.tok ~/.local/share/fcitx5/punctuation/punc.mb.tok
