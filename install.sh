ce1c8337e45382307e9fc4580646e6a33d37c09b
mun luna <115883618+mun-luna@users.noreply.github.com>
2026-06-03 08:28:00 +0000

    Update install.sh
    - shebang is more portable
    - copying will not fail if `~/.local/share/fcitx5` does not exist
    - now copies files recursively instead of individually (why didnt i do that in the first place??)

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo "THIS SCRIPT WILL NOT INSTALL THE NEEDED PACKAGES: which are fcitx5, fcitx5-table-other, and sometimes fcitx5-chinese-addons depending on the distro."
 echo "copying files..."
-cp ./nasin-mun/inputmethod/nasin-mun.conf ~/.local/share/fcitx5/inputmethod/nasin-mun.conf
-cp ./nasin-mun/punctuation/punc.mb.tok ~/.local/share/fcitx5/punctuation/punc.mb.tok
-cp ./nasin-mun/table/nasin-mun.dict ~/.local/share/fcitx5/table/nasin-mun.dict
+mkdir -p ~/.local/share/fcitx5/
+cp -r ./nasin-mun/ ~/.local/share/fcitx5/
 echo "killing fcitx5"
 pkill fcitx5
 echo "you need to enable the nasin mun input method, it is easiest to do this via KDE system settings"
